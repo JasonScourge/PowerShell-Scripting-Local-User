2930774eba76071456ffce658724d59e41274491
Jason Scourge <jasonscourge@hotmail.com>
2019-12-06 21:21:24 +0800

    PowerShell initial commit of creating user

---
+++ localUser.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+# Command input
+Param(
+    [string[]]$ComputerName = $env:COMPUTERNAME,
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Create", "Quit", "Remove", "Rename")]
+    [string]$Command 
+)
+
+# Creating the user
+if($Command -eq "Create") { 
+    [string]$ObjectName = Read-Host -Prompt "Enter a username for the user account"
+    $PasswordForUser = Read-Host -Prompt "Enter a password for user account" -AsSecureString
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($PasswordForUser)
+    $PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) 
+    
+    foreach($Computer in $ComputerName) {
+        if(Test-Connection -ComputerName $Computer -count 1 -Quiet) {
+                try {
+                    $CompObject = [ADSI]"WinNT://$Computer"
+                    $NewObj = $CompObject.Create("User",$ObjectName)
+                    $NewObj.SetPassword($PlainPassword)
+                    $NewObj.SetInfo()
+                    Write-Host "User account with the name $ObjectName created successfully" -ForegroundColor Green
+                } catch {
+                    Write-Warning "Error occurred while creating the user"
+                    Write-Verbose "More details : $_"
+
+                }
+        } else {
+                Write-Warning "$Computer is not online or avaliable"
+        }
+    }
+}
+
+
+# Quitting the script
+if($Command -eq "Quit") {   
+    Write-Host "Quitting application..." -ForegroundColor Red
+}
